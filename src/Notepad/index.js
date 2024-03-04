50449763849646807663a593fdae38022a753b40
AlineEspindola <aline.ae@aluno.ifsc.edu.br>
2024-03-03 22:03:04 -0300

    feat: Adiciona função de adicionar e deletar tarefa

--- src/Notepad/index.js
+++ src/Notepad/index.js
@@ -1,14 +1,26 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useCallback } from 'react';
 import './Notepad.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function Notepad() {
 
   const [tasks, setTasks] = useState([
-    'Dar uma estrela ao repositório',
-    'Seguir Aline',
-    'Dar beijo no Thiago'
+    'Dar uma estrela no repositório'
   ])
+  const [input, setInput] = useState('');
 
+  const taskAdd = useCallback(() => {
+    if(input) {
+       setTasks([...tasks, input]);
+       setInput('');
+     }
+  }, [input, tasks]);
+
+  const taskDelete = useCallback((taskToDelete) => {
+    const newTasks = tasks.filter(task => task != taskToDelete);
+    setTasks(newTasks);
+  }, [tasks]);
+  
   return (
     <div className='notepad'> 
       <div className='notepad__sheet'>
@@ -26,17 +38,27 @@ function Notepad() {
         <div className='sheet__lines'>
           {tasks.map(task => (
             <div className='line'>
-            <div className='line__text'>
-              <input type="checkbox"></input>
-              <p key={task}>{task}</p>
+              <div className='line__content'>
+                <div className='line__text'>
+                  <input type="checkbox"></input>
+                  <p key={task}>{task}</p>
+                </div>
+                <div className='line__button'>
+                  <button onClick={() => taskDelete(task)} className='fa-solid fa-trash'></button>
+                </div>
+              </div>
+              <hr></hr>
             </div>
-            <hr></hr>
-          </div>
           ))}
         </div>
         <div className='sheet__send'>
-          <FontAwesomeIcon icon="fa-solid fa-pen" />
-          <input></input>
+          <div className='send__input'>
+            <i className="fa-solid fa-pen"></i>
+            <input type="text" value={input} placeholder="Escreva uma anotação..." maxLength={36} onChange={e => setInput(e.target.value)}></input>
+          </div>
+          <div className='send__button'>
+            <button className='fa-solid fa-arrow-up' onClick={taskAdd}></button>
+          </div>
         </div>
       </div>
     </div>
